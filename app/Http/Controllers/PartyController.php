c51a2bc6104f6d36cefac2742c4af50ced558365
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-05-13 18:41:26 +0200

    List parties

--- app/Http/Controllers/PartyController.php
+++ app/Http/Controllers/PartyController.php
@@ -44,27 +44,4 @@ class PartyController extends Controller
         $party = Party::where('name', 'like',  $name)->get();
         return $party;
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        
-    }
 }
