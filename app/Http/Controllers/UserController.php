73c6cacfaf51faf56c43265a1e7269510918c9c5
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-05-13 18:39:17 +0200

    Update password bcrypt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,38 +8,6 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -64,16 +32,5 @@ class UserController extends Controller
 
         $user->save();
         return response()->json(['Datos Actualizados con exito']);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
