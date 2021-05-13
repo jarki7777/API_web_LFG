bfe890b4094ca43a7ff1590bd95ea1228a994ebf
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-05-13 18:42:33 +0200

    Create Party_Users

--- app/Http/Controllers/PartyUserController.php
+++ app/Http/Controllers/PartyUserController.php
@@ -8,16 +8,6 @@ use Illuminate\Http\Request;
 class PartyUserController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -33,30 +23,6 @@ class PartyUserController extends Controller
         return $data;
         response()->json(['Te has unido al grupo'], 200);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
     /**
      * Remove the specified resource from storage.
      *
