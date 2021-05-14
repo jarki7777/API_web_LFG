36a7464e7914a8ee66daefa39797a3c1ed8c4b6b
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-05-14 12:34:11 +0200

    Arreglo de rutas

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GamesController;
+use App\Http\Controllers\MessagesController;
 use App\Http\Controllers\PartyController;
 use App\Http\Controllers\PartyUserController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,14 +24,14 @@ Route::group(
         'prefix' => 'auth'
     ],
     function () {
-        Route::post('login', 'App\Http\Controllers\AuthController@login');
-        Route::post('signup', 'App\Http\Controllers\AuthController@signUp');
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('signup', [AuthController::class, 'signUp']);
         Route::group(
             [
                 'middleware' => 'auth:api'
             ],
             function () {
-                Route::get('logout', 'App\Http\Controllers\AuthController@logout');
+                Route::get('logout', [AuthController::class, 'logout']);
             }
         );
     }
@@ -47,7 +48,7 @@ Route::group(
         Route::get('/', [PartyController::class, 'index']);
         Route::post('/', [PartyController::class, 'store']);
         Route::get('/{game_id}', [PartyController::class, 'show']);
-        Route::delete('/party/{id}', [PartyController::class, 'destroy']);        
+        Route::delete('/party/{id}', [PartyController::class, 'destroy']);
     }
 );
 
@@ -73,10 +74,10 @@ Route::group(
         'middleware' => 'auth:api'
     ],
     function () {
-        Route::post('/{party_id}', 'App\Http\Controllers\MessagesController@store');
-        Route::patch('/{party_id}', 'App\Http\Controllers\MessagesController@update');
-        Route::delete('/{party_id}', 'App\Http\Controllers\MessagesController@destroy');
-        Route::get('/{party_id}', 'App\Http\Controllers\MessagesController@index');
+        Route::post('/{party_id}', [MessagesController::class, 'store']);
+        Route::patch('/{party_id}', [MessagesController::class, 'update']);
+        Route::delete('/{party_id}', [MessagesController::class, 'destroy']);
+        Route::get('/{party_id}', [MessagesController::class, 'index']);
     }
 );
 
@@ -84,13 +85,20 @@ Route::group(
 Route::group(
     [
         'prefix' => 'game',
-        'middleware' => ['auth:api', 'scope:admin']
+        'middleware' => ['auth:api']
     ],
     function () {
         Route::get('/', [GamesController::class, 'index']);
-        Route::post('/', [GamesController::class, 'store']);
         Route::get('/{id}', [GamesController::class, 'show']);
-        Route::patch('/{id}', [GamesController::class, 'update']);
-        Route::delete('/{id}', [GamesController::class, 'destroy']);
-    }
+    },
+    Route::group(
+        [
+            'middleware' => ['scope:admin']
+        ],
+        function () {
+            Route::post('/', [GamesController::class, 'store']);
+            Route::patch('/{id}', [GamesController::class, 'update']);
+            Route::delete('/{id}', [GamesController::class, 'destroy']);
+        }
+    )
 );
